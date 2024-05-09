949b5939c1f237a06032a4f49f4a83a65a0e6a8b
joel2982 <jjoel9958@gmail.com>
2024-05-09 17:30:54 +0530

    v2

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import cv2
+import cv2,os
 
-img = cv2.imread('image.jpg')
+img_path = os.path.normpath(input('Enter Image Path : ')) 
+img = cv2.imread(img_path)
 small_img = cv2.resize(img, (0,0), fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
 
 cv2.imshow('Shrinked Image',small_img)
 key = cv2.waitKey()
 
-print(key)
-cv2.imwrite('new_image.jpg',small_img)
+dir_name = os.path.dirname(img_path)
+img_name = os.path.basename(img_path)
+cv2.imwrite(os.path.join(dir_name,f'updated_{img_name}'),small_img)
+
 if __name__ == '__main__':
     pass
